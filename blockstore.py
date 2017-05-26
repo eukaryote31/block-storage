4288d4b5d17e247e74f2aff71358623572bd7815
eukaryote <minerguy31@gmail.com>
2017-05-25 18:34:27 -0600

    Write some more on blockstore

--- blockstore.py
+++ blockstore.py
@@ -1,20 +1,46 @@
 import os.path
 import hashlib
 
+
 class BlockStore:
     def __init__(self, blockdir):
+        assert isinstance(blockdir, str)
         self.blockdir = blockdir
+        os.makedirs(blockdir, exist_ok=True)
 
     def has_block(self, blockhash):
         """Checks if the BlockStore has a block"""
-        return os.path.exists(os.path.join(self.blockdir, blockhash))
+        return os.path.exists(self.get_block_file(blockhash))
 
     def put_block(self, blockdata):
         """Puts a string as a block"""
-        pass
+        assert isinstance(blockdata, str)
+
+        hash = self.get_hash(blockdata)
+
+        # dont put block if it already exists
+        if self.has_block(hash):
+            return hash
+
+        file_output = open(self.get_block_file(hash), "w")
+
+        file_output.write(blockdata)
+
+        file_output.close()
+
+        return hash
 
     def get_hash(self, data):
         """Get a hash for the given data"""
+        assert isinstance(data, str)
         sha256 = hashlib.sha256()
-        sha256.update(data)
+        sha256.update(data.encode("UTF-8"))
         return sha256.hexdigest()
+
+    def get_block_file(self, hash):
+        return os.path.join(self.blockdir, hash)
+
+    def get_block_contents(self, hash):
+        block_file = open(self.get_block_file(hash), "r")
+
+        return block_file.read()
--- test/testblockstore.py
+++ test/testblockstore.py
@@ -1,15 +1,23 @@
 import unittest
+import shutil
 from blockstore import BlockStore
 
+shutil.rmtree("./testblockdir")
 
 class TestBlockStore(unittest.TestCase):
-    bstore = BlockStore("testblockdir")
+    bstore = BlockStore("./testblockdir")
 
     def test_hash(self):
         self.assertEqual("6ca13d52ca70c883e0f0bb101e425a89e8624de51db2d2392593af6a84118090",
                          self.bstore.get_hash("abc123"))
         self.assertEqual("7f83b1657ff1fc53b92dc18148a1d65dfc2d4b1fa3d677284addd200126d9069",
                          self.bstore.get_hash("Hello World!"))
+
+    def test_block_storage(self):
+        hash = self.bstore.put_block("Hello, World!")
+        self.assertEqual("dffd6021bb2bd5b0af676290809ec3a53191dd81c7f70a4b28688a362182986f", hash)
+        contents = self.bstore.get_block_contents(hash)
+        self.assertEqual("Hello, World!", contents)
 
 
 if __name__ == '__main__':
